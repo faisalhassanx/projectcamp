3ee4879f50066f2758ab4bbd081398983e3e6407
Faisal Hassan <faisal.hassanx@gmail.com>
2017-08-23 22:22:11 +0000

    Add plan to registration form and create confirmations controller

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,6 +1,6 @@
 class Tenant < ApplicationRecord
 
-   acts_as_universal_and_determines_tenant
+  acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
   validates :name, presence: true, uniqueness: true
 
